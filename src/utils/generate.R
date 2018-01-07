351a9dacc25361c1d7961899ddda9769746f4409
Carlos Zaldviar Batista <carlos.zaldivar.batista@gmail.com>
2018-01-07 17:47:07 +0100

    Use mean rating for binarization.

--- src/utils/generate.R
+++ src/utils/generate.R
@@ -1,9 +1,9 @@
 # Function for genereting data frame
-generate.generate_empty_df = function(unique_user_id, unique_isbn){
+generate.generate_empty_df = function(unique_user_id, unique_item){
   df = data.frame(user_id=c(unique_user_id))
   
   # Filling data frame with empty rating
-  for (column in unique_isbn){
+  for (column in unique_item){
     df[[toString(column)]] = 0
   }
   return(df)
@@ -11,19 +11,20 @@ generate.generate_empty_df = function(unique_user_id, unique_isbn){
 
 
 # Function for filling data frame
-generate.generate_complete_df = function(df, user_id, isbn, rating){
+generate.generate_complete_df = function(df, user_id, item, rating){
   
   # browser()
   # Write book ratings into data frame
   for (i in seq(length(rating))){
-    cols = toString(isbn[[i]])
+    col = toString(item[[i]])
     row = which(df$user_id == user_id[[i]])
+    m = mean(rating)
     
     # Binarize ratings
-    if (rating[[i]] < 5){
-      df[[cols]][[row]] = 0
+    if (rating[[i]] < m){
+      df[[col]][[row]] = 0
     } else{
-      df[[cols]][[row]] = 1
+      df[[col]][[row]] = 1
     }
   }
   return(df)
